1f8ba6987dea4238a012387f8af601cc874811be
Sonarpratik <99067005+Sonarpratik@users.noreply.github.com>
2022-07-30 20:14:03 +0530

    Initial program

---
+++ java1.js
@@ -0,0 +1,10 @@
+function onClick() {
+    document.getElementById("dropx").classList.toggle("crop");
+}
+
+function flip(){
+    document.getElementById("cross").classList.toggle("open");
+
+    document.getElementById("fit-the-box").classList.toggle("off-menu");
+
+}
